4b6cf117ea1ad2e08137ee70e13a1b71f5bb74af
Lenarafreitas <106668212+Lenarafreitas@users.noreply.github.com>
2022-06-01 08:01:57 -0300

    Create Lenara README.m

---
+++ Lenara README.m
@@ -0,0 +1,15 @@
+- 👋 Hi, I’m @Lenarafreitas
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+Lenarafreitas/Lenarafreitas is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+Obrigada pela oportunidade de poder aprender 
+um pouco de um curso diferente coisas novas 
+isso é muito importante para mim.
+Preciso de uma oportunidade. 
+
